31d432f976ba8d15973701708528768d93250b04
Valentin Famelart <famelartva@eisti.eu>
2017-02-12 17:18:32 +0100

    Add detail page for associations
    Summary:
    Adds a details page for associations
    Links are added on the list page
    
    Test Plan: - `./manage.py test`
    
    Reviewers: terrienale, jamesrapha
    
    Reviewed By: jamesrapha
    
    Subscribers: jordankevi
    
    Maniphest Tasks: T32
    
    Differential Revision: https://phabricator.atilla.org/D23

--- eistiens_net_v3/associations/urls.py
+++ eistiens_net_v3/associations/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.list, name='list'),
+    url(r'^(?P<id>\d+)$', views.details, name='details')
 ]
--- eistiens_net_v3/associations/views.py
+++ eistiens_net_v3/associations/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Association
 from accounts.models import Account
@@ -16,3 +16,8 @@ def list(request):
         'associations/list.html',
         {'assos': assos, 'account': account}
     )
+
+
+def details(request, id):
+    asso = get_object_or_404(Association, pk=id)
+    return render(request, 'associations/details.html', {'asso': asso})
