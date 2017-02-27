bbfc498b5a38918e2a69f03f77eb2b28ed1db9c8
Valentin Famelart <famelartva@eisti.eu>
2017-02-27 18:25:25 +0100

    Add proper Association admin list view
    Summary:
    The Association list view now features the following:
      - Show name, subscription_cost, validation_state, room fields
      - Auto ordering on Associations' name
      - Allows filtering on the validation_state
      - Quick edit Associations' validation state
    
    Closes T57
    
    Test Plan: `./manage.py test`
    
    Reviewers: jamesrapha
    
    Reviewed By: jamesrapha
    
    Subscribers: jordankevi, terrienale
    
    Maniphest Tasks: T57
    
    Differential Revision: https://phabricator.atilla.org/D28

--- eistiens_net_v3/associations/admin.py
+++ eistiens_net_v3/associations/admin.py
@@ -3,7 +3,16 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Association)
+class AssociationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'validation_state', 'subscription_cost', 'room']
+    search_field = ['name', 'room']
+    list_editable = ['validation_state']
+    list_filter = ['validation_state']
+    ordering = ['name']
+
+
+admin.site.register(models.Association, AssociationAdmin)
+
 admin.site.register(models.SocialLink)
 admin.site.register(models.Refusal)
 admin.site.register(models.Membership)
