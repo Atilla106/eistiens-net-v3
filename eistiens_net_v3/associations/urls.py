ec15dfdddd6fb0c57feb9fd4c39682560d44f625
Valentin Famelart <famelartva@eisti.eu>
2018-02-08 10:54:07 +0100

    Add membership check, joining action to association detail page
    Summary:
    - Add an endpoint to allow joining an association
    - Replace the "Join" button with a deactivated one if the user is already a member
    
    Test Plan:
    - Not connected, click a join button, check that you've joined after logging in
    - Already logged in, click a join button, and check that it's deactivated afterwards
    
    Reviewers: corbeaumax, Armteck
    
    Reviewed By: corbeaumax
    
    Maniphest Tasks: T65
    
    Differential Revision: https://phabricator.atilla.org/D34

--- eistiens_net_v3/associations/urls.py
+++ eistiens_net_v3/associations/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.list, name='list'),
     url(r'^(?P<id>\d+)$', views.details, name='details'),
-    url(r'^(?P<id>\d+)/edit$', views.edit, name='edit')
+    url(r'^(?P<id>\d+)/edit$', views.edit, name='edit'),
+    url(r'^join/(?P<id>\d+)$', views.join, name='join'),
 ]
--- eistiens_net_v3/associations/views.py
+++ eistiens_net_v3/associations/views.py
@@ -1,11 +1,15 @@
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
 from django.shortcuts import (
     get_object_or_404,
     HttpResponseRedirect,
+    redirect,
     render)
 
-from .models import Association
+from common.models import SchoolYear
+from .models import Account, Association, Membership
 from .forms import AssociationForm
 
 
@@ -30,11 +34,37 @@ def list(request):
 def details(request, id):
     asso = get_object_or_404(Association, pk=id)
     links = sorted(asso.sociallink_set.all(), key=orderKey)
+    is_member = asso.is_member(request.user)
+
     return render(
         request,
         'associations/details.html',
-        {'asso': asso, 'links': links}
+        {'asso': asso, 'links': links, 'is_member': is_member}
     )
+
+
+# TODO: It will be necessary to check who the authenticated user is
+@login_required
+def join(request, id):
+    asso = get_object_or_404(Association, pk=id)
+    # No need to check if the account exists
+    account = Account.objects.get(user=request.user)
+
+    try:
+        Membership.objects.create(
+            account=account,
+            role='OT',
+            year=SchoolYear.objects.last(),
+            association=asso
+        )
+    except IntegrityError:
+        messages.error(request, "Une erreur s'est produite!")
+
+    messages.success(request,
+                     "Votre inscription est en attente de validation par le " +
+                     "bureau de l'association {}".format(asso.name))
+    return redirect(
+            reverse('associations:details', kwargs={'id': asso.pk}))
 
 
 def edit(request, id):
